c25a716f9c97fb68bc93af1a47612cf68fd2ec91
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-01-14 15:44:13 +0100

    feat(step 5) : mise de seeder lier avec la factory
    [30][DONE]

--- app-teacher-adonisjs/database/migrations/1768394658278_create_teachers_table.ts
+++ app-teacher-adonisjs/database/migrations/1768394658278_create_teachers_table.ts
@@ -1,21 +1,32 @@
-import { TeacherFactory } from '#database/factories/teacher_factory'
-import { BaseSeeder } from '@adonisjs/lucid/seeders'
-import Teacher from '#models/teacher'
-
-export default class extends BaseSeeder {
-  async run() {
-    // Création d'enseignants
-    await Teacher.createMany([
-      {
-        gender: 'M',
-        firstname: 'Grégory',
-        lastname: 'Charmier',
-        nickname: 'GregLeBarbar',
-        origine: "Plateforme de jeux d'échecs",
-        sectionId: 1,
-      },
-    ])
-    // Appel la factory pour créer 10 enseignants
-    await TeacherFactory.createMany(10)
+import { BaseSchema } from '@adonisjs/lucid/schema'
+export default class extends BaseSchema {
+  protected tableName = 'teachers'
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      // Champ genre pour lequel on définit un enum avec pour valeur par défaut
+      table.enum('gender', ['woman', 'man', 'other']).notNullable().defaultTo('woman')
+      // Champ prénom
+      table.string('firstname').notNullable()
+      // Champ nom
+      table.string('lastname').notNullable()
+      // Champ surnom
+      table.string('nickname').notNullable()
+      // Champ origine du prénom
+      table.string('origine').notNullable()
+      // Clé étrangère de la section
+      table
+        .integer('section_id') // Clé étrangère
+        .unsigned() // La clé ne doit pas être négative
+        .references('id') // Référence la colonne `id` de la table `section`
+        .inTable('sections') // Nom de la table de référence
+        .onDelete('CASCADE') // Supprime les teachers si la section est supprimée
+        .onUpdate('CASCADE') // Met à jour la clé étrangère si l'id change
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+  async down() {
+    this.schema.dropTable(this.tableName)
   }
 }
--- app-teacher-adonisjs/database/seeders/teacher_seeder.ts
+++ app-teacher-adonisjs/database/seeders/teacher_seeder.ts
@@ -1,3 +1,4 @@
+import { TeacherFactory } from '#database/factories/teacher_factory'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import Teacher from '#models/teacher'
 export default class extends BaseSeeder {
@@ -12,22 +13,8 @@ export default class extends BaseSeeder {
         origine: "Plateforme de jeux d'échecs",
         sectionId: 1,
       },
-      {
-        gender: 'man',
-        firstname: 'Xavier',
-        lastname: 'Carrel',
-        nickname: 'XCL',
-        origine: 'Sigle ETML',
-        sectionId: 1,
-      },
-      {
-        gender: 'woman',
-        firstname: 'Aurélie',
-        lastname: 'Curchod',
-        nickname: 'ACD',
-        origine: 'Sigle ETML',
-        sectionId: 1,
-      },
     ])
+    // Appel la factory pour créer 10 enseignants
+    await TeacherFactory.createMany(10)
   }
 }
