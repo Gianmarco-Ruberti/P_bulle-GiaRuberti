6f31fa19bc31c11a2f92b39dbb2ab02623a791bb
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-01-14 15:15:59 +0100

    feat(app-teacher) : step 0-5
    [50min][WIP]
    install/home/migration/controleur (DONE)
    seeder(wip)

---
+++ app-teacher-adonisjs/app/controllers/teachers_controller.ts
@@ -0,0 +1,42 @@
+import type { HttpContext } from '@adonisjs/core/http'
+// import { dd } from '@adonisjs/core/services/dumper'
+import Teacher from '../models/teacher.js'
+
+export default class TeachersController {
+  /**
+   * Afficher la liste des enseignants
+   */
+  async index({ view }: HttpContext) {
+    //
+    // Récupérer la liste des enseignants triés par ordre alphabétique sur le nom et le prénom
+    const teachers = await Teacher.query().orderBy('lastname', 'asc').orderBy('firstname', 'asc')
+    // Pour obtenir des infos sur la variable teachers
+    // dd(teachers)
+    // Appel de la vue
+    return view.render('pages/home', { teachers })
+  }
+  /**
+   * Display form to create a new record
+   */
+  async create({}: HttpContext) {}
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {}
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {}
+  /**
+   * Edit individual record
+   */
+  async edit({ params }: HttpContext) {}
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {}
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {}
+}
---
+++ app-teacher-adonisjs/start/routes.ts
@@ -0,0 +1,12 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import TeachersController from '#controllers/teachers_controller'
+import router from '@adonisjs/core/services/router'
+router.get('/', [TeachersController, 'index']).as('home')
