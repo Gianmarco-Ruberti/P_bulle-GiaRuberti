a973c70eb63a991872affbb7ea1be6730318c1c7
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-01-28 15:24:45 +0100

    feat(DB) : création des colonne dans Decks
    [17][DONE]

--- flashcards/database/migrations/1769007509406_create_decks_table.ts
+++ flashcards/database/migrations/1769007509406_create_decks_table.ts
@@ -6,7 +6,16 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.string('Title').notNullable().unique()
+      table.string('Description').notNullable()
+      table.integer('cardsCount').notNullable().unsigned()
+      table
+        .integer('User_id')
+        .unsigned()
+        .references('id')
+        .inTable('cards')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
