8a63305dce88a46142136dd2e72e515c307557fb
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-02-04 14:13:21 +0100

    feat(models) : ajout du comptage de carte
    [17][DONE]

--- flashcards/app/models/deck.ts
+++ flashcards/app/models/deck.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
-// import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
+import Card from '#models/card'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
 // import User from './user.js'
 
 export default class Deck extends BaseModel {
@@ -13,6 +14,9 @@ export default class Deck extends BaseModel {
   @column()
   declare description: string
 
+  @hasMany(() => Card)
+  declare cards: HasMany<typeof Card>
+
   // @column()
   // declare userId: number
 
@@ -24,4 +28,26 @@ export default class Deck extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  //------------
+  /**
+   * 2. L'objet $extras (SANS @column)
+   * Quand on utilise .withCount('cards'), Adonis exécute un SELECT COUNT en SQL.
+   * Il ne peut pas mettre le résultat dans 'cards' (qui est une relation),
+   * donc il le place dans cet objet spécial nommé $extras.
+   */
+  @column()
+  declare $extras: {
+    cards_count?: number
+  }
+
+  /**
+   * 3. Le Getter "Alias"
+   * Pour éviter d'écrire {{ deck.$extras.cards_count }} dans tes vues HTML,
+   * on crée ce raccourci.
+   * Si cards_count n'existe pas (ex: on n'a pas fait le withCount), on renvoie 0.
+   */
+  get cardsCount() {
+    return this.$extras.cards_count || 0
+  }
 }
