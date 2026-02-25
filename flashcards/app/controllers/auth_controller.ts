d98d5948b7ac4b352b5242742fbcbc19be97c258
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-02-25 15:41:49 +0100

    feat(auth) : création du controller du auth
    [52][DONE]
    
    j'ai eu des problème avec l'envois du username qui étais en undefined puis avec la verif des identifiant

--- flashcards/app/controllers/auth_controller.ts
+++ flashcards/app/controllers/auth_controller.ts
@@ -1,14 +1,49 @@
-import Deck from '#models/deck'
 import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import { loginValidator } from '#validators/auth'
 
 export default class AuthController {
-  async index({ view }: HttpContext) {
-    /**
-     * .query() : On démarre une requête SQL
-     * .withCount('cards') : On demande à SQL de compter les lignes liées dans la table 'cards'
-     * Cela génère une sous-requête : (SELECT count(*) FROM cards WHERE deck_id = decks.id)
-     */
-    const decks = await Deck.query().withCount('cards')
-    return view.render('pages/home', { decks })
+  async showLogin({ view }: HttpContext) {
+    console.log(' Affichage de la page de login')
+    return view.render('pages/auth/login')
+  }
+
+  async login({ request, auth, response, session }: HttpContext) {
+    console.log(' Tentative de connexion reçue...')
+    const data = request.all()
+    console.log(` username fourni: ${data.username}`)
+
+    try {
+      // 1. Validation des champs
+      const { username, password } = await request.validateUsing(loginValidator)
+      console.log(' Validation VineJS réussie')
+
+      // 2. Vérification des identifiants
+      const user = await User.verifyCredentials(username, password)
+      console.log(` Utilisateur trouvé: ${user.username} (ID: ${user.id})`)
+
+      // 3. Création de la session
+      await auth.use('web').login(user)
+      console.log(' Session créée, redirection vers /decks')
+
+      return response.redirect().toRoute('decks.index')
+    } catch (error) {
+      console.error(' Échec de la connexion')
+
+      // Si l'erreur vient de la validation (VineJS)
+      if (error.messages) {
+        console.log(' Erreurs de validation:', error.messages)
+      } else {
+        console.log(' Cause probable: Mauvais mot de passe')
+      }
+
+      session.flash('errors', 'Identifiants invalides')
+      return response.redirect().toRoute('deck.index')
+    }
+  }
+
+  async logout({ auth, response }: HttpContext) {
+    await auth.use('web').logout()
+    return response.redirect().toRoute('auth.login')
   }
 }
